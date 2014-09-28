fba7942e952fc43ab8e1b450a8a25a62814d774c
klg71 <klg71@web.de>
2014-09-28 23:33:14 +0200

    Added password condition callback and error Text via AlertDialog in android

--- PassChangeApp/src/core/Website.java
+++ PassChangeApp/src/core/Website.java
@@ -1,16 +1,38 @@
 package core;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public abstract class Website {
 	protected String username;
 	protected String pass;
-	
-	
+	protected Activity activity;
+	protected boolean succesful;
+	protected boolean authenticated;
+
+
+	public boolean isAuthenticated() {
+		return authenticated;
+	}
+
+	public Website(Activity activity){
+		this.activity=activity;
+	}
 	
 	public void initialize(String username, String pass) {
 		this.username = username;
 		this.pass = pass;
 	}
-
+	
+	public boolean isSuccesful(){
+		return succesful;
+	}
+	
 	public abstract void authenticate() throws Exception;
 
 	public abstract void changePassword(String newPass) throws Exception;
@@ -26,4 +48,38 @@ public abstract class Website {
 	public abstract String toString();
 	
 	public abstract Integer getId();
+
+	public abstract boolean validatePassword(String pass);
+	
+	public void displayErrorMessage(String error){
+		final LinearLayout layout = new LinearLayout(activity);	
+		layout.setOrientation(LinearLayout.VERTICAL);
+		
+		TextView errorView= new TextView(activity);
+		layout.addView(errorView);
+		errorView.setText(error);
+		activity.runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+						
+						AlertDialog.Builder builder = 
+						        new AlertDialog.Builder(activity).
+						        setMessage("Error").
+						        setPositiveButton("OK", new OnClickListener() {                     
+						            @Override
+						            public void onClick(DialogInterface dialog, int which) {
+						            }
+						        }).
+						        setView(layout);
+						builder.create().show();
+				    
+				
+			}
+		
+		});	
+	}
+
+	public abstract String getPasswordCondition();
 }
