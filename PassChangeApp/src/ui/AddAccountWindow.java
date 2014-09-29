a59cd93fc9bafac43739e7732128922be106554a
klg71 <klg71@web.de>
2014-09-29 08:16:42 +0200

    Hide keyboard after adding account

--- PassChangeApp/src/ui/AddAccountWindow.java
+++ PassChangeApp/src/ui/AddAccountWindow.java
@@ -11,6 +11,7 @@ import account.Account;
 import account.AccountManager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,34 +22,45 @@ public class AddAccountWindow implements OnClickListener {
 	private AccountManager accountManager;
 	private MainActivity mainActivity;
 	private ArrayList<Website> websites;
-	
-	public AddAccountWindow(AccountManager accountManager,MainActivity mainActivity){
-		this.accountManager=accountManager;
-		this.mainActivity=mainActivity;
-		Button submit=(Button) mainActivity.findViewById(R.id.buttonSubmit);
+
+	public AddAccountWindow(AccountManager accountManager,
+			MainActivity mainActivity) {
+		this.accountManager = accountManager;
+		this.mainActivity = mainActivity;
+		Button submit = (Button) mainActivity.findViewById(R.id.buttonSubmit);
 		submit.setOnClickListener(this);
-		websites=new ArrayList<Website>();
-		for(Map.Entry<String, Website> entry:accountManager.getWebsites().entrySet()){
+		websites = new ArrayList<Website>();
+		for (Map.Entry<String, Website> entry : accountManager.getWebsites()
+				.entrySet()) {
 			websites.add(entry.getValue());
-		} 
+		}
 		Spinner website = (Spinner) mainActivity.findViewById(R.id.spinner1);
-		ArrayAdapter<Website> dataAdapter = new ArrayAdapter<Website>(mainActivity,
-			android.R.layout.simple_spinner_item, websites);
-		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		ArrayAdapter<Website> dataAdapter = new ArrayAdapter<Website>(
+				mainActivity, android.R.layout.simple_spinner_item, websites);
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		website.setAdapter(dataAdapter);
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
-		EditText user,email,pass;
+		EditText user, email, pass;
 		Spinner website;
-		user=(EditText)mainActivity.findViewById(R.id.editUserName);
-		email=(EditText)mainActivity.findViewById(R.id.editEmail);
-		pass=(EditText)mainActivity.findViewById(R.id.editPass);
-		website=(Spinner)mainActivity.findViewById(R.id.spinner1);
-		Calendar temp=Calendar.getInstance();
-		accountManager.addAccount(new Account(user.getText().toString(),email.getText().toString(),pass.getText().toString(), temp, websites.get((int)website.getSelectedItemId()), 10));
+		user = (EditText) mainActivity.findViewById(R.id.editUserName);
+		email = (EditText) mainActivity.findViewById(R.id.editEmail);
+		pass = (EditText) mainActivity.findViewById(R.id.editPass);
+		website = (Spinner) mainActivity.findViewById(R.id.spinner1);
+		Calendar temp = Calendar.getInstance();
+		accountManager.addAccount(new Account(user.getText().toString(), email
+				.getText().toString(), pass.getText().toString(), temp,
+				websites.get((int) website.getSelectedItemId()), 10));
+		// Hide Keyboard
+		InputMethodManager im = (InputMethodManager) mainActivity
+				.getApplicationContext().getSystemService(
+						MainActivity.INPUT_METHOD_SERVICE);
+		im.hideSoftInputFromWindow(mainActivity.getWindow().getDecorView()
+				.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 		mainActivity.setContentView(R.layout.activity_main);
 		mainActivity.refreshAccountList();
 		mainActivity.setChildWindowActive(false);
