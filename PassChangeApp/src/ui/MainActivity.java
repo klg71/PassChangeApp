340d6eaf5056f69c3d131aa6087e64434c5bbbee
klg71 <klg71@web.de>
2014-10-26 19:34:28 +0100

    falsed debug flag

--- PassChangeApp/src/ui/MainActivity.java
+++ PassChangeApp/src/ui/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends Activity implements OnItemLongClickListener,
 		android.widget.PopupMenu.OnMenuItemClickListener,
 		android.content.DialogInterface.OnClickListener, OnItemClickListener {
 
-	public final static boolean DEBUG_ACTIVATED = true;
+	public final static boolean DEBUG_ACTIVATED = false;
 
 	private LoginManager loginManager;
 	private HashMap<String, Website> websites;
