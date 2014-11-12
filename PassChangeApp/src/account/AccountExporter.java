33b0ab6fd64b2a88442406fc8448e2114854f16b
klg71 <klg71@web.de>
2014-11-12 15:05:09 +0100

    some minor improvements

--- PassChangeApp/src/account/AccountExporter.java
+++ PassChangeApp/src/account/AccountExporter.java
@@ -5,7 +5,9 @@ import generator.Crypt;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.URLEncoder;
+
 import core.RequestType;
 import core.WebClient;
 
@@ -19,17 +21,23 @@ public class AccountExporter {
 		salt = "1234567890ABCDEFGHIJKLMONPQRSTUVWXYZ";
 		webClient = new WebClient();
 	}
-
+	
+	public String toHex(String arg) throws UnsupportedEncodingException{
+		return String.format("%x",new BigInteger(1,arg.getBytes("ISO-8859-1")));
+	}
+	
 	public String exportAccount(Account account) throws Exception {
 		String hash = "";
 		try {
 			ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
-			Crypt.encode(account.getActualPassword().getBytes(), byteArrayOutputStream, Crypt.generateKey(masterPass, salt));
+			Crypt.encode(account.getActualPassword().getBytes("ISO-8859-1"), byteArrayOutputStream, Crypt.generateKey(masterPass, salt));
 			String encryptedPass=byteArrayOutputStream.toString("ISO-8859-1");
+			System.out.println("Encrypted Pass:"+encryptedPass);
+			//encryptedPass=new String(Crypt.decode(new ByteArrayInputStream(encryptedPass.getBytes("ISO-8859-1")),Crypt.generateKey(masterPass, salt)),"ISO-8859-1");
 			hash = webClient
 					.sendRequest(
 							"http://klg71.us.to/PassChangeServer/sendPassword.php",
-							RequestType.POST, "password="+URLEncoder.encode(encryptedPass, "ISO-8859-1"),
+							RequestType.POST, "password="+toHex(URLEncoder.encode(encryptedPass, "ISO-8859-1")),
 							"exportAnswer", false);
 			if(hash.contains("failed")){
 				throw new Exception("Export failed");
