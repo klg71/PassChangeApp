3a2c356e79d94afca9376fcb671ea097f8e4c083
klg71 <klg71@web.de>
2014-11-27 23:09:48 +0100

    added compatibility for fragment ui

--- PassChangeApp/src/ui/AddAccountWindow.java
+++ PassChangeApp/src/ui/AddAccountWindow.java
@@ -20,40 +20,44 @@ import android.widget.Spinner;
 public class AddAccountWindow implements OnClickListener {
 
 	private AccountManager accountManager;
-	private MainActivity mainActivity;
+	private MainFragmentActivity mainActivity;
 	private ArrayList<Website> websites;
+	private View mainView;
 
 	public AddAccountWindow(AccountManager accountManager,
-			MainActivity mainActivity) {
+			MainFragmentActivity mainFragmentActivity, View mainView) {
 		this.accountManager = accountManager;
-		this.mainActivity = mainActivity;
-		Button submit = (Button) mainActivity.findViewById(R.id.buttonSubmit);
+		this.mainView=mainView;
+		this.mainActivity = mainFragmentActivity;
+		Button submit = (Button)  mainView.findViewById(R.id.buttonSubmit);
 		submit.setOnClickListener(this);
 		websites = new ArrayList<Website>();
 		for (Map.Entry<String, Website> entry : accountManager.getWebsites()
 				.entrySet()) {
 			websites.add(entry.getValue());
 		}
-		Spinner website = (Spinner) mainActivity.findViewById(R.id.spinner1);
+		Spinner website = (Spinner)  mainView.findViewById(R.id.spinner1);
 		WebsiteSpinnerAdapter dataAdapter = new WebsiteSpinnerAdapter(
 				mainActivity, android.R.layout.simple_spinner_item, websites);
 		dataAdapter
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		website.setAdapter(dataAdapter);
-		EditText expire = (EditText) mainActivity.findViewById(R.id.editExpire);
+		EditText expire = (EditText)  mainView.findViewById(R.id.editExpire);
 		expire.setText("10");
 
 	}
+
+	
 
 	@Override
 	public void onClick(View v) {
 		EditText user, email, pass,expire;
 		Spinner website;
-		user = (EditText) mainActivity.findViewById(R.id.editUserName);
-		email = (EditText) mainActivity.findViewById(R.id.editEmail);
-		pass = (EditText) mainActivity.findViewById(R.id.editPass);
-		website = (Spinner) mainActivity.findViewById(R.id.spinner1);
-		expire = (EditText) mainActivity.findViewById(R.id.editExpire);
+		user = (EditText)  mainView.findViewById(R.id.editUserName);
+		email = (EditText)  mainView.findViewById(R.id.editEmail);
+		pass = (EditText)  mainView.findViewById(R.id.editPass);
+		website = (Spinner)  mainView.findViewById(R.id.spinner1);
+		expire = (EditText)  mainView.findViewById(R.id.editExpire);
 		Calendar temp = Calendar.getInstance();
 		Account tempAcc=accountManager.addAccount(new Account(user.getText().toString(), email
 				.getText().toString(), pass.getText().toString(), temp,
@@ -66,7 +70,5 @@ public class AddAccountWindow implements OnClickListener {
 		im.hideSoftInputFromWindow(mainActivity.getWindow().getDecorView()
 				.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
 		mainActivity.setContentView(R.layout.activity_main);
-		mainActivity.refreshAccountList();
-		mainActivity.setChildWindowActive(false);
 	}
 }
