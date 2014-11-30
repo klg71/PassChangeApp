941799b35ebc60514d6a1cdf842a6a51b09dc621
Administrator <admin@example.com>
2014-11-30 11:31:37 +0100

    fixed some bugs loads now reliable

--- PassChangeApp/src/ui/WebViewFragment.java
+++ PassChangeApp/src/ui/WebViewFragment.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnSystemUiVisibilityChangeListener;
@@ -14,7 +15,7 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
-public class WebViewFragment extends CustomFragment{
+public class WebViewFragment extends CustomFragment {
 	private WebView webView;
 	private View mainView;
 	private Account account;
@@ -25,12 +26,16 @@ public class WebViewFragment extends CustomFragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// webView=new WebView(getActivity());
+
 		mainView = inflater.inflate(R.layout.webview, container, false);
-		spinner=(ProgressBar) mainView.findViewById(R.id.progressBar1);
+		spinner = (ProgressBar) mainView.findViewById(R.id.progressBar1);
 		spinner.setVisibility(View.VISIBLE);
 		webView = (WebView) mainView.findViewById(R.id.webView1);
 		webView.setVisibility(View.GONE);
-			account.openBrowser(webView, getActivity(),this);
+		if (!firstLoad) {
+			Log.e("DEBUG", "onCreateView");
+			account.openBrowser(webView, getActivity(), this);
+		}
 		firstLoad = false;
 		// refresh();
 		return mainView;
@@ -62,9 +67,9 @@ public class WebViewFragment extends CustomFragment{
 
 	}
 
-	public void removeProgressBar(){
+	public void removeProgressBar() {
 		spinner.setVisibility(View.GONE);
-		
+
 	}
 
 }
