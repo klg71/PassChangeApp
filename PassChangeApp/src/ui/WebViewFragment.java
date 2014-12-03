e949e790796a083c5ef7cb1da517f84f9007e576
klg71 <klg71@web.de>
2014-12-03 22:55:35 +0100

    one instance per each fragment

--- PassChangeApp/src/ui/WebViewFragment.java
+++ PassChangeApp/src/ui/WebViewFragment.java
@@ -3,21 +3,24 @@ package ui;
 import com.passchange.passchangeapp.R;
 
 import account.Account;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnSystemUiVisibilityChangeListener;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 public class WebViewFragment extends CustomFragment {
 	private WebView webView;
-	private View mainView;
+	private LinearLayout mainView;
 	private Account account;
 	private ProgressBar spinner;
 	private boolean firstLoad;
@@ -26,13 +29,22 @@ public class WebViewFragment extends CustomFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// webView=new WebView(getActivity());
-
-		mainView = inflater.inflate(R.layout.webview, container, false);
-		spinner = (ProgressBar) mainView.findViewById(R.id.progressBar1);
+		mainView=new LinearLayout(getActivity());
+		mainView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		mainView.setOrientation(LinearLayout.VERTICAL);
+		//mainView = inflater.inflate(R.layout.webview, container, false);
+		//spinner = (ProgressBar) mainView.findViewById(R.id.progressBar1);
+		spinner=new ProgressBar(getActivity());
 		spinner.setVisibility(View.VISIBLE);
-		webView = (WebView) mainView.findViewById(R.id.webView1);
+		//LayoutParams params=new LayoutParams(Gravity.CENTER);
+		//spinner.setLayoutParams(params);
+		
+		//webView = (WebView) mainView.findViewById(R.id.webView1);
+		webView=new WebView(getActivity());
 		webView.setVisibility(View.GONE);
-		if (!firstLoad) {
+		mainView.addView(spinner);
+		mainView.addView(webView);
+		if (firstLoad) {
 			Log.e("DEBUG", "onCreateView");
 			account.openBrowser(webView, getActivity(), this);
 		}
