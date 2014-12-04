92ba56ef43f706d8efbf9b4e70acdbbb5b65d946
klg71 <klg71@web.de>
2014-12-04 19:16:33 +0100

    custom exception created

---
+++ PassChangeApp/src/exceptions/AccountCredentialWrongException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class AccountCredentialWrongException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AccountCredentialWrongException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
